390245087ce9af3920e18e0416a09a94e4e47e57
Ben Fanning fanninggg@gmail.com <fanninggg@gmail.com>
2021-07-14 12:47:13 +0100

    Heavier font weight for skill subtitles, CV updates for email

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,18 +7,18 @@ export const Footer = () => {
   return (
     <div className="footer portfolio-section">
       <FooterLogoSVG />
-      <p className="max-width text-center">If you've got this far maybe you'd like to know more about me? <br /> We should <motion.a whileHover={{ scale: 1.05 }} href="mailto:fanninggg@gmail.com" target="_blank">book in a chat</motion.a></p>
+      <p className="max-width text-center">If you've got this far maybe you'd like to know more about me? <br /> We should <motion.a whileHover={{ scale: 1.05 }} href="mailto:fanning@hey.com" target="_blank">book in a chat</motion.a></p>
       <div className="social-links">
-        <motion.a whileHover={{ scale: 1.1 }} href="https://www.linkedin.com/in/ben-fanning/" className="social-link" target="_blank">
+        <motion.a whileHover={{ scale: 1.1 }} href="https://www.linkedin.com/in/ben-fanning/" className="social-link" rel="noreferrer" target="_blank">
           <LinkedinSVG />
         </motion.a>
-        <motion.a whileHover={{ scale: 1.1 }} href="https://github.com/fanninggg" className="social-link" target="_blank">
+        <motion.a whileHover={{ scale: 1.1 }} href="https://github.com/fanninggg" className="social-link" rel="noreferrer" target="_blank">
           <GithubSVG />
         </motion.a>
-        <motion.a whileHover={{ scale: 1.1 }} href="https://www.instagram.com/benfanning_/" className="social-link" target="_blank">
+        <motion.a whileHover={{ scale: 1.1 }} href="https://www.instagram.com/benfanning_/" className="social-link" rel="noreferrer" target="_blank">
           <InstagramSVG />
         </motion.a>
-        <motion.a whileHover={{ scale: 1.1 }} href="https://dribbble.com/fanninggg" className="social-link" target="_blank">
+        <motion.a whileHover={{ scale: 1.1 }} href="https://dribbble.com/fanninggg" className="social-link" rel="noreferrer" target="_blank">
           <DribbbleSVG />
         </motion.a>
       </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,7 +9,6 @@ export const Projects = () => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth)
 
   const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
 
   const handleReize = () => {
     setWindowWidth(window.innerWidth)
@@ -101,7 +100,7 @@ export const Projects = () => {
   return (
     <div className="projects max-width portfolio-section">
       <h2>My Recent Work</h2>
-      <p>Here are a few of the projects I've worked on recently. Want to see more? <motion.a whileHover={{ scale: 1.05 }} href="mailto:fanninggg@gmail.com" className="linear-link" target="_blank">Email Me</motion.a></p>
+      <p>Here are a few of the projects I've worked on recently. Want to see more? <motion.a whileHover={{ scale: 1.05 }} href="mailto:fanning@hey.com" className="linear-link" target="_blank">Email Me</motion.a></p>
       {renderProjects()}
       <Modal show={show} onHide={handleClose} animation={false} className="project-modal">
         <Modal.Body>
@@ -114,7 +113,7 @@ export const Projects = () => {
           <div className="modal-divider"></div>
           <p className="project-description">{selectedProject ? selectedProject.description : ''}</p>
           <div className="modal-actions">
-            <motion.a whileHover={{ scale: 1.1 }} className="btn-linear" target="_blank" href={selectedProject ? selectedProject.href : ''}>Visit Website</motion.a>
+            <motion.a whileHover={{ scale: 1.1 }} className="btn-linear" rel="noreferrer" target="_blank" href={selectedProject ? selectedProject.href : ''}>Visit Website</motion.a>
             <svg className="close-btn" onClick={handleClose} width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M11.8325 10.0001L19.6203 2.21215C20.1271 1.70557 20.1271 0.88651 19.6203 0.379933C19.1137 -0.126644 18.2947 -0.126644 17.7881 0.379933L10.0001 8.16793L2.21233 0.379933C1.70552 -0.126644 0.886688 -0.126644 0.380111 0.379933C-0.126704 0.88651 -0.126704 1.70557 0.380111 2.21215L8.16787 10.0001L0.380111 17.7881C-0.126704 18.2947 -0.126704 19.1138 0.380111 19.6204C0.632569 19.8731 0.964513 20 1.29622 20C1.62793 20 1.95963 19.8731 2.21233 19.6204L10.0001 11.8324L17.7881 19.6204C18.0408 19.8731 18.3725 20 18.7042 20C19.0359 20 19.3676 19.8731 19.6203 19.6204C20.1271 19.1138 20.1271 18.2947 19.6203 17.7881L11.8325 10.0001Z" fill="white" fillOpacity="0.6" />
             </svg>
